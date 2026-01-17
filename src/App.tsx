cd15fce2c887d80ccb0571a56e52a2527d919d72
SaintKiri <SaintKiri@proton.me>
2026-01-17 17:29:10 -0500

    Migrated from `.css` to `.scss`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 import SiteNavigationBar from "./SiteNavigationBar";
 
@@ -9,22 +9,31 @@ function App() {
 
       <div className="App">
         {/* TODO: 
+        - https://noaheakin.medium.com/changing-the-default-react-browser-tab-title-and-icon-1240239d92d3
         - Decide on a color theme, don't go monochrome
         - Add a colored divider to each section. between heading and its other elements
         - Think about what to add in the `Experience` section
         - MAKE PROGRESS ON PRESENTABLE PROJECTS
          */}
 
+        <section id='summary'>
+          <h1>Jack Song</h1>
+        </section>
+
         <section id='experience'>
-          <h1>Experience</h1>
+          <h2>Experience</h2>
         </section>
 
         <section id='projects'>
-          <h1>Projects</h1>
+          <h2>Projects</h2>
+        </section>
+
+        <section id='education'>
+          <h2>Education</h2>
         </section>
 
         <section id='contact'>
-          <h1 >Contact</h1>
+          <h2 >Contact</h2>
           <p>Send me an email at <a href="mailto:jis110@pitt.edu">jis110@pitt.edu</a> and I will get back to you ASAP!</p>
         </section>
       </div>
--- src/SiteNavigationBar.tsx
+++ src/SiteNavigationBar.tsx
@@ -2,23 +2,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min"; 
-
 function SiteNavigationBar() {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" sticky='top'>
       <Container>
         <Navbar.Brand href='#home'>Jack S</Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href='#experience'>Experience</Nav.Link>
-            <Nav.Link href='#projects'>Projects</Nav.Link>
-            <Nav.Link href='#contact'>Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className="ms-auto">
+          <Nav.Link href='#experience'>Experience</Nav.Link>
+          <Nav.Link href='#projects'>Projects</Nav.Link>
+          <Nav.Link href='#contact'>Contact</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
