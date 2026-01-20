4ea4d111a1a616f5d78b1dcc123964cc64978ce4
SaintKiri <SaintKiri@proton.me>
2026-01-20 01:53:51 -0500

    Using `Grid` for experience info

--- src/ExperienceCard.tsx
+++ src/ExperienceCard.tsx
@@ -1,21 +1,53 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import LinkIcon from '@mui/icons-material/Link';
 
 import type { SimpleCard } from './types/SimpleCard';
 
 function ExperienceCard({ exp }: { exp: SimpleCard }) {
   return (
     <Card variant="outlined">
-      <CardContent className='info'>
-        <h5>{exp.companyName}</h5>
-        <p>{exp.date}</p>
-        {exp.location && <p>{exp.location}</p>}
-        <a href={exp.companyURL[1]}>{exp.companyURL[0]}</a>
-      </CardContent>
-      <CardContent className='Description'>
-        <h6>{exp.descriptionTitle}</h6>
-        <p>{exp.description}</p>
-      </CardContent>
+      <Grid container spacing={2}>
+        <Grid size={5}>
+          <CardContent className='info'>
+            <h5>{exp.companyName}</h5>
+
+            <Grid container>
+              <Grid size={1}>
+                <CalendarMonthIcon />
+              </Grid>
+              <Grid size={11}>
+                <p>{exp.date}</p>
+              </Grid>
+
+              {exp.location && <>
+                <Grid size={1}>
+                  <LocationPinIcon />
+                </Grid><Grid size={11}>
+                  <p>{exp.location}</p>
+                </Grid>
+              </>}
+
+              <Grid size={1}>
+                <LinkIcon />
+              </Grid>
+              <Grid size={11}>
+                <a href={exp.companyURL[1]}>{exp.companyURL[0]}</a>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Grid>
+        <Grid size={7}>
+          <CardContent className='description'>
+            <h6>{exp.descriptionTitle}</h6>
+            <p>{exp.description}</p>
+          </CardContent>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
